28f5cb4f97a392be5ea29a7dd3f0b2168a44f122
jeroen-wj <jeroen.bechtold@gmail.com>
2022-04-01 22:48:11 +0200

    fix world creation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from bw4t.BW4TWorld import DEFAULT_WORLDSETTINGS, BW4TWorld
+from bw4t.BW4TWorld import  BW4TWorld, createwordsettings
 from bw4t.statistics import Statistics
 from agents1.BW4TBaselineAgent import BaseLineAgent
 from agents1.BW4THuman import Human
@@ -10,18 +10,19 @@ from agents1.Lazy import Lazy
 This runs a single session. You have to log in on localhost:3000 and 
 press the start button in god mode to start the session.
 """
-
-if __name__ == "__main__":
-    agents = [
-      # {'name': 'agent1', 'botclass': BaseLineAgent, 'settings': {}},
-        {'name':'Liar', 'botclass':Liar, 'settings':{}},
-        {'name':'human', 'botclass':Human, 'settings':{}},
-        {'name': 'Lazy', 'botclass': Lazy, 'settings': {}}
-        ]
-
-    print("Started world...")
-    wordsetttings = DEFAULT_WORLDSETTINGS
-    wordsetttings['tick_duration'] = 0.1 #set lower for faster speed, default seems 0.1
-    world=BW4TWorld(agents, worldsettings=wordsetttings).run()
-    print("DONE!")
-    print(Statistics(world.getLogger().getFileName()))
+for i in range(1,100): 
+  if __name__ == "__main__":
+      agents = [
+        # {'name': 'agent1', 'botclass': BaseLineAgent, 'settings': {}},
+          {'name':'Liar', 'botclass':Liar, 'settings':{}},
+          {'name':'human', 'botclass':Human, 'settings':{}},
+          # {'name': 'Lazy', 'botclass': Lazy, 'settings': {}}
+          ]
+       
+      print("Started world...")
+      wordsetttings = createwordsettings()
+      wordsetttings['tick_duration'] = 0.00000001 #set lower for faster speed, default seems 0.1
+      world=BW4TWorld(agents, worldsettings=wordsetttings).run()
+      wordsetttings = None
+      print("DONE!")
+      print(Statistics(world.getLogger().getFileName()))
